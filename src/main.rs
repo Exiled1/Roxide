e51f131ab50f8cdf90525436a0f099296872f4e7
exiled1 <rudyperalta831@gmail.com>
2022-09-27 15:53:19 -0700

    Asked Jonathan, got help setting up linker stuff, linker failing b/c its LLVM lld, work on linker still in progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
-use core::{panic::PanicInfo};
+use core::{panic::PanicInfo, arch::global_asm};
+
+global_asm!(include_str!("boot.S"));
+
 
 // Need a panic handler to avoid compiler errors, as it's a required item for no_std
 #[panic_handler]
@@ -12,9 +15,9 @@ fn panic(_info: &PanicInfo) -> ! {
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn kernel_main() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
-
+    
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
